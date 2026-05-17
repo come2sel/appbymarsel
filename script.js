05605ed21431b0093cf6445371d115d5580b2575
come2sel <marselinacatur@gmail.com>
2026-05-17 15:36:36 +0700

    Update pl

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const products = {
   ],
   "NETFLIX": [
   { desc: "1p1u 1 hari", price: "Rp 4.000", cat: "Streaming" },
-  { desc: "1p1u 7 hari", price: "Rp 11.000", cat: "Streaming" }
+  { desc: "1p1u 7 hari", price: "Rp 12.000", cat: "Streaming" }
   ],
   "VIU": [
     { desc: "Private 1 Bulan", price: "Rp 4.000", cat: "Streaming" },
