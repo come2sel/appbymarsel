85847f6c89320fd8d90ff1d1092245d63418aace
come2sel <marselinacatur@gmail.com>
2026-06-08 12:09:26 +0700

    updt pl cc

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const products = {
     { desc: "Team Invite 1 Tahun", price: "Rp 10.000", cat: "Editing" }
   ],
   "CAPCUT PRO": [
-    { desc: "Sharing 7 Hari", price: "Rp 7.000", cat: "Editing" },
-    { desc: "Private 7 Hari", price: "Rp 11.000", cat: "Editing" },
+    { desc: "Sharing 7 Hari", price: "Rp 8.000", cat: "Editing" },
+    { desc: "Private 7 Hari", price: "Rp 15.000", cat: "Editing" },
   ],
   "NETFLIX": [
   { desc: "1p1u 1 hari", price: "Rp 4.000", cat: "Streaming" },
